924a172053f0dedf401754a5219e184a47c083d3
Jere Lantz <lantz.jere@gmail.com>
2025-07-26 22:19:05 +0300

    reading user input

--- c1_treehouse/src/main.rs
+++ c1_treehouse/src/main.rs
@@ -1,3 +1,12 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello. What's your name?");
+
+    let mut user_name = String::new();
+
+    stdin().read_line(&mut user_name)
+        .expect("Failed to read line");
+
+    println!("Hello {user_name}");
 }
