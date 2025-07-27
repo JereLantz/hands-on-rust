43ac6b4555f80830ee052607446ffbcdfb7f8f39
Jere Lantz <lantz.jere@gmail.com>
2025-07-27 23:40:45 +0300

    started simple game state design

--- p1_flappy/src/main.rs
+++ p1_flappy/src/main.rs
@@ -4,13 +4,61 @@
  */
 use bracket_lib::prelude::*;
 
-struct State {}
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self{
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm){
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {},
+            }
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm){
+        //TODO:
+        self.mode = GameMode::End;
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm){
+        //TODO:
+        ctx.cls();
+        ctx.print_centered(8, "You are dead!");
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),        }
     }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
 }
 
 fn main() -> BError{
@@ -18,5 +66,5 @@ fn main() -> BError{
         .with_title("Flappy Dragon")
         .build()?;
 
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
