61551ee955cdf8da214dc79768238d4286390722
Jere Lantz <lantz.jere@gmail.com>
2025-07-27 11:01:57 +0300

    structs for information storage and iterator for searching array

--- c1_treehouse/src/main.rs
+++ c1_treehouse/src/main.rs
@@ -1,5 +1,24 @@
 use std::io::stdin;
 
+struct Visitor{
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self{
+        // Koska tässä ei oo puolipistettä, se tarkoittaa "implicit return"
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string()
+        }
+    }
+
+    fn greet(&self){
+        println!("{}", self.greeting);
+    }
+}
+
 fn ask_for_input() -> String {
     let mut input = String::new();
 
@@ -10,25 +29,20 @@ fn ask_for_input() -> String {
 }
 
 fn main() {
-    let visitor_list = ["bert", "bob", "jorma"];
+    let visitor_list = [
+        Visitor::new("bob", "Waddup bob"),
+        Visitor::new("ben", "Hey, Ben. How's it hanging?"),
+    ];
     println!("Hello. What's your name?");
 
     let user_name = ask_for_input();
 
     println!("Hello {}", user_name);
 
-    let mut allowed = false;
-    for name in &visitor_list{
-        if name == &user_name {
-            allowed = true;
-            break;
-        }
-    }
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == user_name);
 
-    if allowed {
-        println!("Welcome to the Treehouse, {user_name}");
-    }
-    else{
-        println!("Sorry. You cannot enter because your name is not in the visitors list");
+    match known_visitor {
+        Some(visitor) => visitor.greet(),
+        None => println!("Sorry. You are not in the visitors list."),
     }
 }
